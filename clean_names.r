7bbf78a25e6d0fb4b38916bf03661c50a6a6798f
injuryepi <injury.epi@gmail.com>
2018-04-19 09:49:47 -0700

    Update clean_names.r

--- clean_names.r
+++ clean_names.r
@@ -1,5 +1,8 @@
 clean_names <- compose(
-	function(x) gsub("(_)(?=_*\\1)|_$", "", x, perl = T), # repeat "_" and end "_"
-	function(x) gsub("\\W", "_", x), # not [A-Za-z0-9_] and replace with "_"
-	function(x) gsub("\\(.+\\)", "", x),# parenthesis and its contents
+	# remove repeat "_" and extreme "_"
+	function(x) gsub("(_)(?=_*\\1)|^_|_$", "", x, perl = T), 
+	# not [A-Za-z0-9_] and replace with "_"
+	function(x) gsub("\\W", "_", x), 
+	# parenthesis and its contents
+	function(x) gsub("\\(.+\\)", "", x),
 	tolower)
