8b60f64ad1c4b80f858395d21ec1d28e74fd2393
injuryepi <injury.epi@gmail.com>
2018-08-09 17:38:49 -0700

    Update add_age8.r

--- add_age8.r
+++ add_age8.r
@@ -8,11 +8,11 @@ add_age8 <- function(data, age) {
 	
 	age <- data %>% pull(!!age)
 	
-	agecut8 <- c(10, 17, 24, 34, 44, 54, 64, 84, max(age, na.rm = T))
+	agecut8 <- c(10, 17, 24, 34, 44, 54, 64, 74, max(age, na.rm = T))
 	int8 <- classIntervals(age, n = 8, style = "fixed", fixedBreaks = agecut8, intervalClosure = "right")
 	
 	agegrp8 <- as.factor(findCols(int8))
 	
-	data %>% mutate(agegrp8 = agegrp8, age8 = fct_recode(agegrp8, `10_17` = "1",`18-24` = "2", `25-34` = "3", `35-44` = "4", `45-54` = "5", `55-64` = "6",`65-84` = "7", `85+` = "8"))
+	data %>% mutate(agegrp8 = agegrp8, age8 = fct_recode(agegrp8, `10_17` = "1",`18-24` = "2", `25-34` = "3", `35-44` = "4", `45-54` = "5", `55-64` = "6",`65-74` = "7", `75+` = "8"))
 
 }
