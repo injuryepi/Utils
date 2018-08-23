50fb88c862cdf0bfaeed85e4d6bb88c82558a6ec
injuryepi <injury.epi@gmail.com>
2018-08-23 09:02:11 -0700

    Create suicide_.r
    icd10 suicide codes based on  International Classification of External Causes of Injury (ICECI)

---
+++ suicide_.r
@@ -0,0 +1,2 @@
+# from International Classification of External Causes of Injury (ICECI)
+suicide_ <- "^X78|^X71|^X80|^X76|^X77|^X72|^X73|^X74|^X82|^X60|^X61|^X62|^X63|^X64|^X65|^X66|^X67|^X68|^X69|^X79|^X70|^X75|^X81|^Y870|^X83|^X84"
