c2c8be49ac4a39aa41fad8f78049cb462f4304d8
injuryepi <injury.epi@gmail.com>
2018-09-18 12:42:08 -0700

    Update snake_case2.r
    remove extra "_"

--- snake_case2.r
+++ snake_case2.r
@@ -1,4 +1,6 @@
 snake_case2 <- purrr::compose(tolower, 
+# remove repeat "_" and extreme "_"
+function(x) gsub("(_)(?=_*\\1)|^_|_$", "", x, perl = T),
 function(x) gsub("(?<=(\\w))([A-Z])", "_\\2", x, perl = TRUE),
 function(x) gsub("(?>([A-Z]))([A-Z]+)", "\\1\\L\\2", x, perl = TRUE),
 function(x) gsub("\\s+", "", x, perl = TRUE))
