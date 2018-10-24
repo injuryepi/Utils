0c867f60d7856d7133842ddb07981c6c7d2412ad
injuryepi <injury.epi@gmail.com>
2018-10-23 17:09:57 -0700

    Update get_ed_stimulant.r

--- get_ed_stimulant.r
+++ get_ed_stimulant.r
@@ -1,6 +1,6 @@
 # get stimulant api
 
-get_ed_stimulant <- function(username, password, start_date, end_date, version = 2, site_no = 934, user_id = 515) {
+get_ed_stimulant <- function(username, password, start_date, end_date, version = 2, site_no, user_id) {
   require(httr, quietly = T)
   require(jsonlite, quietly = T)
   require(glue, quietly = T)
