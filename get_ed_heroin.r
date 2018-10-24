26bb6067d41da815809647a25e98ca7b3d9c737f
injuryepi <injury.epi@gmail.com>
2018-10-23 17:09:12 -0700

    Update get_ed_heroin.r

--- get_ed_heroin.r
+++ get_ed_heroin.r
@@ -1,6 +1,6 @@
 # get heroin api
 
-get_ed_heroin <- function(username, password, start_date, end_date, version = 4, site_no = 934, user_id = 515) {
+get_ed_heroin <- function(username, password, start_date, end_date, version = 4, site_no, user_id) {
   require(httr, quietly = T)
   require(jsonlite, quietly = T)
   require(glue, quietly = T)
