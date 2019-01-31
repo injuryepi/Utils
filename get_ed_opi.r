b8f514f526c54f7ecdc638e0889b71aa5ad5fbcb
injuryepi <injury.epi@gmail.com>
2019-01-31 07:53:08 -0800

    Update get_ed_opi.r
    optional select fields

--- get_ed_opi.r
+++ get_ed_opi.r
@@ -1,6 +1,6 @@
 # get opioid api
 
-get_ed_opi <- function(username, password, start_date, end_date, version = 2, site_no, user_id) {
+get_ed_opi <- function(username, password, start_date, end_date, version = 2, site_no = 934, user_id = 515, ...) {
   require(httr, quietly = T)
   require(jsonlite, quietly = T)
   require(glue, quietly = T)
@@ -19,6 +19,26 @@ get_ed_opi <- function(username, password, start_date, end_date, version = 2, si
   url <- gsub("(%20v)\\d+", glue("\\1{version}"), url)
   url <- gsub("(&site=)\\d+", glue("\\1{site_no}"), url)
   url <- gsub("(&userId=)\\d+", glue("\\1{user_id}"), url)
+  
+  ## select fields
+  
+  f_field <- function(url, ...){
+    
+    if(!length(list(...))){
+      field <- NULL
+    } 
+    else { 
+      field <- paste(paste0("&field=",list(...)), collapse="")   
+    }
+    
+    gsub("&detector", paste0(field, "&detector"), url)
+    
+    
+  }
+  
+  url <- f_field(url = url, ...)
+  
+  ##
   
   set_config(config(ssl_verifypeer = 0L))
   resp <- httr::GET(url = url, authenticate(username , password ))
